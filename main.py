407b4ff142d42cca3deb9a4e8a1d0b1c571eea44
AaronRaszz <rasz.aaron@gmail.com>
2026-03-23 11:55:22 -0600

    Inicio, Proyecto

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+Proyecto1
+Autor: Aarón
+
+"""
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
+import sys
+from src.ventana_principal import VentanaPrincipal
+"""
+
+
+
+"""
+if __name__ == "__main__":
+
+    Aplicacion = QApplication (sys.argv)
+    Ventana = VentanaPrincipal ()
+    Ventana.show()
+    Aplicacion.exec()
